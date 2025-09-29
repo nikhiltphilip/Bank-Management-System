df61eef371f68a2baab715e61f699b59486cb8d7
Null <null@debian.xyz>
2025-09-29 14:27:49 +0530

    enhance security and transaction validations

--- db.py
+++ db.py
@@ -1,19 +1,30 @@
 import mariadb
-import traceback
 import sys
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 def get_db_connection():
-    try:
-        conn = mariadb.connect(
-            user="null", password="1234", host="localhost", port=3306, database="bank"
-        )
-        return conn
-    except mariadb.Error as e:
-        print(f"Database connection error: {e}")
-        traceback.print_exc()
-        return None
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    port_str = os.getenv("DB_PORT")
+    database = os.getenv("DB_NAME")
 
+    if not all([user, password, host, port_str, database]):
+        raise ValueError("Missing required environment variables in .env file")
+
+    assert port_str is not None
+    port = int(port_str)
+
+    conn = mariadb.connect(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=database
+    )
+    return conn
 
 def get_cursor(conn):
     try:
